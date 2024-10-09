7ff468c693e78174485f650e8f98fdb5fab9f6c9
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-09 12:32:57 -0400

    Making update so the user does not need to add .txt file in the story name

--- BedtimeStories/src/main/java/com/pluralsight/Main.java
+++ BedtimeStories/src/main/java/com/pluralsight/Main.java
@@ -6,19 +6,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter story name: ");
-        String storyName = scanner.nextLine().replace(' ', '_');
+        System.out.println("Enter story name: ");
+        String storyName = scanner.nextLine().replace(' ', '_') + ".txt";
 
         try{
-            FileInputStream story = new FileInputStream(storyName);
-            Scanner fileScanner = new Scanner(story);
-            String input;
+            FileInputStream file = new FileInputStream(storyName);
+            Scanner fileScanner = new Scanner(file);
+            String story;
             int lineCount = 1;
+            
             while(fileScanner.hasNextLine()){
-                input = fileScanner.nextLine();
-                System.out.println(lineCount++ + ": " + input);
+                story = fileScanner.nextLine();
+                System.out.println(lineCount++ + ": " + story);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
