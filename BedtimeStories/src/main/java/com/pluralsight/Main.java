8cd57ac7c7fd0824dc22af044b136fdc654747cb
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-09 12:23:43 -0400

    Finishing the project

--- BedtimeStories/src/main/java/com/pluralsight/Main.java
+++ BedtimeStories/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,26 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter story name: ");
+        String storyName = scanner.nextLine().replace(' ', '_');
+
+        try{
+            FileInputStream story = new FileInputStream(storyName);
+            Scanner fileScanner = new Scanner(story);
+            String input;
+            int lineCount = 1;
+            while(fileScanner.hasNextLine()){
+                input = fileScanner.nextLine();
+                System.out.println(lineCount++ + ": " + input);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
+
