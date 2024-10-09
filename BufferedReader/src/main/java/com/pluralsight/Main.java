27721216e480300fb4bb9ad8e25d5ff1f1bac217
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-09 15:57:58 -0400

    Creating a .txt file

--- BufferedReader/src/main/java/com/pluralsight/Main.java
+++ BufferedReader/src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
@@ -13,6 +14,9 @@ public class Main {
         bufReader.readLine();
         String input;
 
+        FileWriter fw = new FileWriter("payroll-act-2024.txt");
+        fw.write("id|name|gross pay\n");
+
         while((input = bufReader.readLine()) != null) {
             String[] tokes = input.split(Pattern.quote("|"));
 
@@ -20,16 +24,17 @@ public class Main {
             String name = tokes[1];
             float hoursWorked = Float.parseFloat(tokes[2]);
             float payRate = Float.parseFloat(tokes[3]);
-
-//            System.out.println(employeeId);
-//            System.out.println(name);
-//            System.out.println(hoursWorked);
-//            System.out.println(payRate);
+            
             Employee theEmp = new Employee(employeeId, name, hoursWorked, payRate);
             displayEmplouee(theEmp);
+
+            String outputData = theEmp.getEmployeeId() + '|' + theEmp.getName() + '|' + theEmp.getGrossPay()  +"\n";
+            System.out.println(outputData);
+            fw.write(outputData);
         }
 
         bufReader.close();
+        fw.close();
     }
     private static void displayEmplouee(Employee employee){
         System.out.printf("%d: %s Pay this period: %.2f%n",employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
