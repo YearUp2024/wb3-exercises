30205e4dcf3387e1f4f6868307639d6899b4639e
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-11 07:10:20 -0400

    Storing information in .csv file

--- PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,17 @@
 package com.pluralsight;
 
+import java.io.FileWriter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            FileWriter fileReader = new FileWriter("newFile.csv");
+
+            fileReader.write("Skills: \n");
+            fileReader.write("HTML, CSS, JavaScript");
+            fileReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
